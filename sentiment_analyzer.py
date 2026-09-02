d24915e37694b5f37a95ff51cc3d364d82e8ce3d
Rihab Harfi <rihabharfi16@gmail.com>
2026-09-02 20:33:06 +0100

    Create sentiment_analyzer.py

---
+++ sentiment_analyzer.py
@@ -0,0 +1,21 @@
+texts = [
+    "I love this movie",
+    "This is amazing",
+    "I really enjoyed this",
+    "What a wonderful experience",
+    "I hate this movie",
+    "This is terrible",
+    "I really disliked this",
+    "What a horrible experience"
+]
+
+labels = [
+    "positive",
+    "positive",
+    "positive",
+    "positive",
+    "negative",
+    "negative",
+    "negative",
+    "negative"
+]
